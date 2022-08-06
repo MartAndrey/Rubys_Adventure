7ce7b9f6c962964b393fec947ead9d9464878c77
Andrey Martin <jeffersonandreyma12@gmail.com>
2022-08-05 19:35:19 -0500

    Aim

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,18 +7,26 @@ public class PlayerController : MonoBehaviour
     // ======================MOVEMENT======================
     [SerializeField] float speed = 3.0f;
 
+    // =========================AIM========================
+    [SerializeField] Transform aim;
+    [SerializeField] Camera aimCamera;
+    Vector2 lookDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+
+        MoveAim();
     }
 
+    // ======================MOVEMENT======================
     void Move()
     {
         float horizontal = Input.GetAxis("Horizontal");
@@ -29,6 +37,13 @@ public class PlayerController : MonoBehaviour
         position.x += speed * horizontal * Time.deltaTime;
         position.y += speed * vertical * Time.deltaTime;
 
-        transform.position = position;        
+        transform.position = position;
+    }
+
+    // =========================AIM========================
+    void MoveAim()
+    {
+        lookDirection = aimCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        aim.position = transform.position + (Vector3) lookDirection.normalized;
     }
 }
