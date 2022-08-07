9b4d91862f1daf4f7b6f3f61684b458ac78064f4
Andrey Martin <jeffersonandreyma12@gmail.com>
2022-08-07 16:39:57 -0500

    Target

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,9 +7,22 @@ public class Bullet : MonoBehaviour
     // ======================MOVEMENT======================
     [SerializeField] float speed = 5f;
 
+    void Start()
+    {
+        Destroy(gameObject, 5);
+    }
     // Update is called once per frame
     void Update()
     {
         transform.position += speed * transform.right * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            other.GetComponent<Enemy>().TakeDamage();
+            Destroy(gameObject);
+        }
     }
 }
