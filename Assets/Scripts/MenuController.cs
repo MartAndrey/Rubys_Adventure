fcf2adc5d8b6750a9c05e457bd68e086f66b0fe3
Andrey Martin <jeffersonandreyma12@gmail.com>
2022-08-28 18:02:24 -0500

    Adding function mute and unmute

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
+    [SerializeField] Image imageSound;
+
+    [SerializeField] Sprite muteEnable;
+    [SerializeField] Sprite muteDiseable;
+    
     public void Play()
     {
         GameManager.Instance.MenuScene();
@@ -11,10 +17,24 @@ public class MenuController : MonoBehaviour
 
     public void Exit()
     {
-        #if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
-        #else
-            Application.Quit();
-        #endif
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
+    }
+
+    public void MuteAudio()
+    {
+        if (AudioListener.volume == 1)
+        {
+            imageSound.sprite = muteDiseable;
+            AudioListener.volume = 0;
+        }
+        else if (AudioListener.volume == 0)
+        {
+            imageSound.sprite = muteEnable;
+            AudioListener.volume = 1;
+        }
     }
 }
